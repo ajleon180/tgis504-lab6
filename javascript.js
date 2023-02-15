8e3e06be23a4e5d45678580ba68641cfd6d90446
ajleon180 <115597155+ajleon180@users.noreply.github.com>
2023-02-14 22:01:24 -0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -13,6 +13,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var drawnItems = L.featureGroup().addTo(map);
 
 //Add a control that allows the user to draw lines, points, and polygons on the map.
+//I'm leaving in the option to draw points, lines, and polygons.  The idea is that, eventually, people will be able to use the app to create points for bird sightings, as well as lines and polygons to demark areas where birds are commonly found (such as streams and wetlands).
 new L.Control.Draw({
     draw : {
         polygon : true,
@@ -30,12 +31,15 @@ new L.Control.Draw({
 //Add popups for the user to enter data into their drawn features.
 function createFormPopup() {
     var popupContent = 
-        '<form>' + 
+        '<form id=popup>' + 
         'Species of Bird:<br><input type="text" id="input_bird"><br>' +
         'Number of Birds:<br><input type="text" id="input_num"><br>' +
         'Date of Sighting:<br><input type="date" id="input_date"><br>' +
         'User\'s Name:<br><input type="text" id="input_name"><br>' +
-        'Take a Photo:<br><input type="file" id="input_photo" capture="environment" accept="image/*"><br>' +
+        //I added inputs for users to add audio, video, and photos of the birds.  I tried to get the buttons to appear on the same line as the label, but was unsuccessful.
+        'Take a Photo:<input type="file" id="input_photo" capture="environment" accept="image/*"><br>' +
+        'Take a Video:<input type="file" id="input_video" capture="environment" accept="video/*"><br>' +
+        'Take an Audio Recording:<br><input type="file" id="input_audio" capture="environment" accept="audio/*"><br>' +
         '<input type="button" value="Submit" id="submit">' + 
         '</form>'
     drawnItems.bindPopup(popupContent).openPopup();
@@ -56,12 +60,15 @@ function setData(e) {
         var enteredDate = document.getElementById("input_date").value;
         var enteredUsername = document.getElementById("input_name").value;
         var enteredPhoto = document.getElementById("input_photo").value;
-        // Print entered information in the console log.
+        var enteredVideo = document.getElementById("input_video").value;
+        var enteredAudio = document.getElementById("input_audio").value;
         console.log(enteredBird);
         console.log(enteredBirdNum);
         console.log(enteredDate);
         console.log(enteredUsername);
         console.log(enteredPhoto);
+        console.log(enteredVideo);
+        console.log(enteredAudio);
         // Get and print GeoJSON for each drawn layer.
         drawnItems.eachLayer(function(layer) {
             var drawing = JSON.stringify(layer.toGeoJSON().geometry);
